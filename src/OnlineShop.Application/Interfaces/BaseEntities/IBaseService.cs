e454b0fbf6f3b8f2e65f95ddd1ca6255bc39b8cd
Mihai Buga <77620547+mihaibuga@users.noreply.github.com>
2024-05-27 20:56:59 +0200

    Renamed parameter

--- src/OnlineShop.Application/Interfaces/BaseEntities/IBaseService.cs
+++ src/OnlineShop.Application/Interfaces/BaseEntities/IBaseService.cs
@@ -2,7 +2,7 @@
 {
     public interface IBaseService<TDto, CreateTRequestDto>
     {
-        Task<TDto> CreateAsync(CreateTRequestDto brandDto);
+        Task<TDto> CreateAsync(CreateTRequestDto baseEntityDto);
         Task<List<TDto>> GetAllAsync();
         Task<TDto?> GetByIdAsync(Guid id);
         Task<TDto> UpdateAsync();
