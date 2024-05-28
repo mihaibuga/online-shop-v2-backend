3412a1c194f27e127013560fbe9685fea560df14
Mihai Buga <77620547+mihaibuga@users.noreply.github.com>
2024-05-28 11:42:52 +0200

    Created users controller

---
+++ src/OnlineShop.API/Controllers/UsersController.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+using OnlineShop.Application.DTOs.Auth;
+using OnlineShop.Application.DTOs.Users;
+
+using OnlineShop.Domain.Entities.Users;
+using OnlineShop.Domain.Interfaces.Services.Authentication;
+
+namespace OnlineShop.API.Controllers
+{
+	[Route("api/users")]
+	public class UsersController : ApiControllerBase
+	{
+		private readonly UserManager<AppUser> _userManager;
+		private readonly ITokenService _tokenService;
+
+		public UsersController(UserManager<AppUser> userManager, ITokenService tokenService)
+		{
+			_userManager = userManager;
+			_tokenService = tokenService;
+		}
+
+		[HttpPost]
+		[Authorize]
+		public async Task<IActionResult> Create([FromBody] CreateUserRequestDto userDto)
+		{
+			try
+			{
+				if (!ModelState.IsValid)
+					return BadRequest(ModelState);
+
+				var appUser = new AppUser
+				{
+					UserName = userDto.Username,
+					Email = userDto.Email
+				};
+
+				if (userDto.Password != null)
+				{
+					var result = await _userManager.CreateAsync(appUser, userDto.Password);
+
+					if (result.Succeeded)
+					{
+						return Ok(
+								new NewUserDto
+								{
+									UserName = appUser.UserName,
+									Email = appUser.Email,
+									Token = _tokenService.CreateToken(appUser)
+								}
+							);
+					}
+					else
+					{
+						return StatusCode(500, result.Errors);
+					}
+				}
+				else
+				{
+					return StatusCode(500, "An internal problem has occured! Please try again later!");
+				}
+
+			}
+			catch (Exception e)
+			{
+				return StatusCode(500, e);
+			}
+		}
+
+		[HttpGet]
+		[Authorize]
+		public IActionResult GetAll()
+		{
+			var users = _userManager.Users;
+			return Ok(users);
+		}
+
+		[HttpGet("{id:Guid}")]
+		[Authorize]
+		public async Task<IActionResult> GetById([FromRoute] string id)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+
+			var existingUser = await _userManager.FindByIdAsync(id);
+
+			if (existingUser == null)
+			{
+				return NotFound("The user does not exist.");
+			}
+
+			return Ok(existingUser);
+		}
+
+		[HttpDelete]
+		[Route("{id:Guid}")]
+		[Authorize]
+		public async Task<IActionResult> Delete([FromRoute] string id)
+		{
+			try
+			{
+				if (!ModelState.IsValid)
+				{
+					return BadRequest(ModelState);
+				}
+
+				var existingUser = await _userManager.FindByIdAsync(id);
+
+				if (existingUser == null)
+				{
+					return NotFound("The user does not exist.");
+				}
+				else
+				{
+					var result = await _userManager.DeleteAsync(existingUser);
+
+					if (result.Succeeded)
+					{
+						return NoContent();
+					}
+					else
+					{
+						return StatusCode(500, result.Errors);
+					}
+				}
+
+			}
+			catch (Exception e)
+			{
+				return StatusCode(500, e);
+			}
+		}
+	}
+}
---
+++ src/OnlineShop.Application/DTOs/Users/CreateUserRequestDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace OnlineShop.Application.DTOs.Users
+{
+	public class CreateUserRequestDto
+	{
+		[Required]
+		public string? Username { get; set; }
+
+		[Required]
+		[EmailAddress]
+		[RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "Email is not valid")]
+		public string? Email { get; set; }
+
+		[Required]
+		public string? Password { get; set; }
+	}
+}
